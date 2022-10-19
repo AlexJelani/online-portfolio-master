cac31bba1987e8261d754a3c24890d4389612923
Jelani Alexander <jelanialexander82@gmail.com>
2022-10-19 18:02:43 +0900

    added font size to li items

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 //TODO
 
-//Change Jquery to here https://codepen.io/erikkjon88/pen/MGWzwV
-//add active button to languagechange button https://stackoverflow.com/questions/41476916/jquery-make-pressed-button-active-and-all-other-buttons-inactive
-//Add email to main by merge
+
 
 var arrLang = {
   "en-gb": {
@@ -18,7 +16,7 @@ var arrLang = {
   "zh-tw": {
     ABOUT: "私について",
     PROJECT: "プロジェクト",
-    CONTACT: "連絡",
+    CONTACT: "お問い合わせ",
     NAME: "アレキサンダー　ジェラーニ",
     JOB: "フルスタックエンジニア",
     //cant add lots of data
