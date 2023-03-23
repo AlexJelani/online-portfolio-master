5847e19dc6781858d9e46769efb13e88922b0b55
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-24 08:16:24 +0900

    coverted js to vite react

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
