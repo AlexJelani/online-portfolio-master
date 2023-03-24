d0eb6d869500372bade3a505c350b82faf1403e6
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-25 07:51:49 +0900

    changed skills color

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import {skills} from "../data";
 
 const Skills = () => {
   return (
-    <section className=" bg-secondary py-12" >
+    <section className=" bg-primary py-12" >
         <div className="grid grid-cols-8 md:grid-flow-row">
           {skills.map((skill, index) => {
             return (
