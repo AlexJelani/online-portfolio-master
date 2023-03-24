449f438f5e144d419b9029524e613894d75afe5b
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-24 10:46:25 +0900

    added gap between Socials

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -8,7 +8,7 @@ const Socials = () => {
     <ul className='flex space-6-x'>
       {social.map((item, index)=>{
         return <li className='flex justify-center items-center key={index}'>
-            <a className='text-base'   href={item.href}>
+            <a className='text-base mx-1'   href={item.href}>
                 {item.icon}
             </a>
         </li>
