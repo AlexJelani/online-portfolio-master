b017fb299e8bcdba474e0845dd44a9973f0ae6e4
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-24 13:53:18 +0900

    deleted a container

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,6 @@ import {skills} from "../data";
 const Skills = () => {
   return (
     <section className=" bg-secondary py-12" >
-      <container className="mx-auto">
         <div className="grid grid-cols-8 md:grid-flow-row">
           {skills.map((skill, index) => {
             return (
@@ -16,7 +15,6 @@ const Skills = () => {
             );
           })}
         </div>
-      </container>
     </section>
   );
 };
