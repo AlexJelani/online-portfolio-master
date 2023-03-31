08d374a618d1090fbf2ef0106e4773cf6fe50e2b
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-31 15:16:35 +0900

    added resume to tab

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
             See projects
           </a>
           <a
-            href="/#projects"
+            href="./assets/JelaniAlexanderResume.pdf" target="_blank" rel="noopener noreferrer"
             class="btn bg-accent border-2 border-[#7477FF] text-white px-4 py-2 hover:bg-transparent ml-2"
           >
             Resume
