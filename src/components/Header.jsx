bc8af244d1b2e31ea5bf88a563db560550a2a15d
Jelani Alexander <jelanialexander82@gmail.com>
2023-10-28 12:56:24 +0900

    added resume to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
           </li>
           <li>
             <a
-              href="https://drive.google.com/file/d/1eZLN8hAv98u4NlNq_-Sk3usdMViNLbwE/view?usp=share_link"
+              href="https://drive.google.com/file/d/1v7MXr1S2OkllLBMu3EDsvRkGzmxIKr9g/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
             >
